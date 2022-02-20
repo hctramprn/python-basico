e7fb349a2ceefe1f90f4b1296aeaebbf42de04f0
Héctor Amparán <hctramprn@gmail.com>
2022-02-19 23:38:02 -0600

    Se agrega la opción para tener 3 monedas

--- Conversor de monedas/conversor de monedas.py
+++ Conversor de monedas/conversor de monedas.py
@@ -1,8 +1,43 @@
-#Programa que convierte pesos a dólares
-pesos = input("¿Cuántos pesos mexicanos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 21
-dolares = pesos / valor_dolar
-dolares = round(dolares,2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+'''Programa que convierte pesos a dólares'''
+
+#Las comillas dobles repetidas 3 veces nos permiten guardar un string con el formato que nosotros le demos en el editor
+menu = """"
+Bienvenido al conversor de monedas 💵
+
+1. Pesos mexicanos
+2. Pesos colombianos
+3. Pesos argentinos
+
+Elige una opción: """
+
+#Recibimos el input del usuario y lo convertimos a enteros
+opcion = int(input(menu))
+
+#Se hace el cálculo de pesos a dolares
+if opcion == 1:
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 21
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos colombianos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 3931
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 106
+    dolares = pesos / valor_dolar
+    dolares = round(dolares,2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print('Ingresa una opción correcta por favor')
+
