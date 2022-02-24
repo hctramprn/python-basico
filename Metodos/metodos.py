e728b29dcd2e2b9119bbe38da23843c9f69fbd1e
Héctor Amparán <hctramprn@gmail.com>
2022-02-23 20:19:05 -0600

    Se agrega un archivo con ejemplos de métodos para strings

---
+++ Metodos/metodos.py
@@ -0,0 +1,17 @@
+nombre = input('¿Cuál es tu nombre?: ')
+
+#El método upper es una función que pone en mayúsculas el string que le estemos pasando
+nombre.upper()
+
+#El método lower es una función que pone en minúsculas el string que le estemos pasando
+nombre.lower()
+
+#El método capitalize es una función que capitaliza la primera letra del string que le estemos pasando
+nombre = nombre.capitalize()
+
+#El método strip elimina los espacios basura que pueden estar al principio o al final de una cadena de caracteres
+nombre = nombre.strip()
+
+#El método replace reemplaza los caracteres que coincidan con el primer parámetro con el caracter del segundo parámetro
+nombre = nombre.replace('o','a')
+
